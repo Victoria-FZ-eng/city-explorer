f909148a4b03c24b67ddbbf6a059b9b18b5d51dc
Victoria-FZ-eng <vzulof@gmail.com>
2021-06-13 17:38:55 +0300

    some styling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import axios from 'axios';
+import Image from 'react-bootstrap/Image';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+
 
 class App extends React.Component{
 
@@ -49,13 +54,20 @@ class App extends React.Component{
     return (
       <div>
         <h1>City Explorer</h1>
-        <form onSubmit={this.getMap}>
-          <input type='text' placeholder='Type the location' name='location' ></input>
-          <input type='submit' value='Explore'></input>
-        </form>
+        <Form onSubmit={this.getMap}>
+          <Form.Control size="lg" type="text" placeholder="Type the location" name='location' />
+          <Button  variant="secondary" size="lg" type='submit'>
+            Explore
+          </Button>
+          </Form>
+        
+        
+       
 
-        <p>{this.state.locationData.display_name}</p>
-        {this.state.showMap && <img src={`https://maps.locationiq.com/v3/staticmap?key=pk.d4d8710c42b8bcdc64be1378a880880b&center=${this.state.locationData.lat},${this.state.locationData.lon}`} alt={`Map for ${this.state.display_name}`}/>}
+        <h3>{this.state.locationData.display_name}</h3>
+        
+        {this.state.showMap && <Image src={`https://maps.locationiq.com/v3/staticmap?key=pk.d4d8710c42b8bcdc64be1378a880880b&center=${this.state.locationData.lat},${this.state.locationData.lon}`} alt={`Map for ${this.state.display_name}`} roundedCircle  />}
+       
         { this.state.displayMsg && this.state.msg}
 
       </div>
